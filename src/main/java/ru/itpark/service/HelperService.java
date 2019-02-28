baf7d07bd03f6b36044e0945317d4796fb8fc19b
ilmir <luke3skystone@gmail.com>
2019-02-28 22:21:34 +0300

    fix pom.xml

--- src/main/java/ru/itpark/service/HelperService.java
+++ src/main/java/ru/itpark/service/HelperService.java
@@ -1,6 +1,6 @@
 package ru.itpark.service;
 
-public class HelperService {
+class HelperService {
   public String initials(String name){
       String[] parts = name.split(" ");
       return parts[0].substring(0, 1) + parts[1].substring(0, 1);
