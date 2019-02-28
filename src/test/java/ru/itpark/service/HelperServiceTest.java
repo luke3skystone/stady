8bf74a2927db82eb0098e0d4a3e9f3467e425227
ilmir <luke3skystone@gmail.com>
2019-02-28 17:50:15 +0300

    Helper Service for initials generation + auto test

---
+++ src/test/java/ru/itpark/service/HelperServiceTest.java
@@ -0,0 +1,18 @@
+package ru.itpark.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelperServiceTest {
+
+    @Test
+    void initials() {
+        {
+            HelperService service = new HelperService();
+            String actual = service.initials("Vasiliy Petrov");
+            String expected = "VP";
+            assertEquals(expected, actual);
+        }
+    }
+}
